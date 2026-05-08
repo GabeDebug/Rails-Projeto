731e1f3e190820c58d43d434f54ec0032c302564
GabeDebug <gabrielmessiasc0d3@gmail.com>
2026-05-08 04:00:29 -0300

    Passando a routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :products
   get "produtos/index"
+
+  Rails.application.Route.draw do
+    resources: produtos, controller: 'products'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
 
